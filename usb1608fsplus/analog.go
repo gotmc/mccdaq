22eed7133105087b82f6f43f7efbfc2d9dda95b8
Matthew Rankin <matthew@cumulusware.com>
2016-03-15 17:23:40 -0500

    Fix analog scan debug mode
    Based on other drivers, it appears that the debug mode is enabled using
    0x20 not 0x10.

--- usb1608fsplus/analog.go
+++ usb1608fsplus/analog.go
@@ -57,42 +57,40 @@ func (daq *USB1608FSPlus) ReadAnalogInput(channel int, rng voltageRange) (uint, 
 // pacer_period = [40 MHz / (A/D frequency)] - 1
 //
 /*
-   If pacer_period is set to 0, the device does not generate an A/D
-   clock.  It uses the SYNC pin as an input and the user must
-   provide the pacer source.  The A/Ds acquire data on every rising
-   edge of SYNC; the maximum allowable input frequency is 100 kHz.
+	 If pacer_period is set to 0, the device does not generate an A/D clock.  It
+	 uses the SYNC pin as an input and the user must provide the pacer source.
+	 The A/Ds acquire data on every rising edge of SYNC; the maximum allowable
+	 input frequency is 100 kHz.
 
-   The data will be returned in packets untilizing a bulk endpoint.
-   The data will be in the format:
+	 The data will be returned in packets untilizing a bulk endpoint.  The data
+	 will be in the format:
 
    lowchannel sample 0: lowchannel + 1 sample 0: ... : hichannel sample 0
    lowchannel sample 1: lowchannel + 1 sample 1: ... : hichannel sample 1
    ...
    lowchannel sample n: lowchannel + 1 sample n: ... : hichannel sample n
 
-   The scan will not begin until the AInScan Start command is sent
-   and any trigger conditions are met.  Data will be sent until
-   reaching the specified count or an usbAInScanStop_USB1608FS_Plus()
-   command is sent.
+	 The scan will not begin until the AInScan Start command is sent and any
+	 trigger conditions are met.  Data will be sent until reaching the specified
+	 count or an usbAInScanStop_USB1608FS_Plus() command is sent.
 
-   The external trigger may be used to start the scan.  If enabled,
-   the device will wait until the appropriate trigger condition is
-   detected than begin sampling data at the specified rate.  No
-   packets will be sent until the trigger is detected.
+	 The external trigger may be used to start the scan.  If enabled, the device
+	 will wait until the appropriate trigger condition is detected than begin
+	 sampling data at the specified rate.  No packets will be sent until the
+	 trigger is detected.
 
-   In block transfer mode, the data is sent in 64-byte packets as
-   soon as data is available from the A/D.  In immediate transfer
-   mode, the data is sent after each scan, resulting in packets that
-   are 1-8 samples (2-16 bytes) long.  This mode should only be used
-   for low pacer rates, typically under 100 Hz, because overruns
-   will occur if the rate is too high.
+	 In block transfer mode, the data is sent in 64-byte packets as soon as data
+	 is available from the A/D.  In immediate transfer mode, the data is sent
+	 after each scan, resulting in packets that are 1-8 samples (2-16 bytes)
+	 long.  This mode should only be used for low pacer rates, typically under
+	 100 Hz, because overruns will occur if the rate is too high.
 
-   There is a 32,768 sample FIFO, and scans under 32 kS can be
-   performed at up to 100 kHz*8 channels without overrun.
+	 There is a 32,768 sample FIFO, and scans under 32 kS can be performed at up
+	 to 100 kHz*8 channels without overrun.
 
-   Overruns are indicated by the device stalling the bulk endpoint
-   during the scan.  The host may read the status to verify and ust
-   clear the stall condition before further scan can be performed.
+	 Overruns are indicated by the device stalling the bulk endpoint during the
+	 scan.  The host may read the status to verify and clear the stall condition
+	 before further scan can be performed.
 */
 func (daq *USB1608FSPlus) StartAnalogScan(
 	numScans int, frequency float64, channels byte, options byte,
